8534396085600992d78eca76ce4051f921e3bc90
Abhishek <nit.abhi85@gmail.com>
2016-02-01 23:14:21 +0530

    Adding account api

--- api/models/Account.js
+++ api/models/Account.js
@@ -1,19 +1,33 @@
 /**
-* User.js
+* Account.js
 *
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
-* User – id, name, created at, modified at, status
+* username, password, userid, status
 */
 
 module.exports = {
 
     attributes: {
-        name: {
+        username: {
             type: 'string',
             required : true
         },
 
+        password: {
+            type : 'integer',
+            required : true,
+            enum : [0, 1, 2],
+            defaultsTo : 1
+        },
+        
+        userid: {
+            type : 'integer',
+            required : true,
+            enum : [0, 1, 2],
+            defaultsTo : 1
+        },
+        
         status: {
             type : 'integer',
             required : true,
