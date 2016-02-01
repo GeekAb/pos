122c6cd229b74e373aa84f645b398f97f13fd370
Abhishek <nit.abhi85@gmail.com>
2016-02-01 23:34:31 +0530

    Default controller changed and index page moved to login page

--- config/routes.js
+++ config/routes.js
@@ -24,17 +24,14 @@ module.exports.routes = {
 
   /***************************************************************************
   *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
+  * view located at `views/login/index.ejs` our home page.                   *
   *                                                                          *
   ***************************************************************************/
 
   '/': {
-    view: 'homepage'
-  }
+    controller: 'LoginController',
+    action: 'index'
+  },
 
   /***************************************************************************
   *                                                                          *
