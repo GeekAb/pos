64a894f8c35586b9fa24592c7ccec54367e92bee
Abhishek <nit.abhi85@gmail.com>
2016-02-02 18:48:43 +0530

    Changes

--- api/controllers/LoginController.js
+++ api/controllers/LoginController.js
@@ -5,7 +5,31 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
+global.jQuery = require('jquery');
+var bootstrap = require("bootstrap-material-design");
+
+module.exports = {   
+    
+    
+    index: function (req, res) {
+        res.view(null, {
+            title: 'Home'
+        });
+    },
+    
+    create: function (req, res) {
+        return res.json({
+            todo: 'Not implemented yet!'
+        });
+    },
+
+    /**
+     * CommentController.destroy()
+     */
+    destroy: function (req, res) {
+        return res.json({
+            todo: 'Not implemented yet!'
+        });
+    },
 };
 
